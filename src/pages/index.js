c7dda633ec7ca8a82e1aa9b8cf2c42a7e6a646c3
Kurtis Kemple <kurtiskemple@gmail.com>
2019-09-10 10:04:17 -0400

    add newsletter popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 import Carousel from 'nuka-carousel'
+import { DialogOverlay, DialogContent } from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 import {
   ContentContainer,
@@ -11,11 +14,6 @@ import {
   SectionSubTitle,
   SectionTitle,
   Layout,
-  Coaches,
-  Coach,
-  CoachTitle,
-  CoachDescription,
-  CoachImg,
 } from '../components'
 
 const HeroContainer = styled('div')`
@@ -82,6 +80,31 @@ const GetStartedButton = styled(Link)`
   margin-top: 16px;
 `
 
+const NewsLetterButton = styled(`a`)`
+  padding: 12px 24px;
+  background-color: #cd3c33;
+  color: white;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 12px;
+  border-radius: 3px;
+  display: block;
+  width: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 16px;
+  text-align: center;
+`
+
+const CloseDialogButton = styled('button')`
+  outline: none;
+  border: none;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  cursor: pointer;
+`
+
 const Embed = styled(`iframe`)`
   margin-left: auto;
   margin-right: auto;
@@ -98,120 +121,150 @@ const BtwbImageContainer = styled(`div`)`
   margin-right: auto;
 `
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <HeroContainer>
-      <HeroImageContainer>
-        <Carousel
-          withoutControls
-          autoplay
-          wrapAround
-          speed={600}
-          autoplayInterval={5000}
-          transitionMode="fade"
-        >
-          {data.sliderImages.edges.map(({ node: image }) => {
-            return (
-              <HeroImage key={image.name} fluid={image.childImageSharp.fluid} />
-            )
-          })}
-        </Carousel>
-      </HeroImageContainer>
-      <HeroImageOverlay />
-    </HeroContainer>
-    <SectionSpacer />
-    <ContentContainer>
-      <SectionTitle>Welcome to CrossFit Krypton</SectionTitle>
-      <SectionSubTitle>
-        We're different from your ordinary gym and gym membership
-      </SectionSubTitle>
-      <SectionContent>
-        When you come to our gym, the only thing we expect from you is a
-        willingness to learn and challenge yourself, as well as a motivation and
-        desire to better yourself each day. During each group class, we will
-        provide personal instruction, coaching, programming (exercises, reps,
-        time domains, etc.) and nutritional advice (if requested). We are
-        committed to helping every member of our gym to reach their goals, and
-        if you commit to bringing your best effort and attitude each day, I can
-        guarantee that you will exceed any realistic fitness expectations you
-        have.
-        <GetStartedButton to="/contact-us">Get Started</GetStartedButton>
-      </SectionContent>
-      <SectionSpacer />
-      <SectionTitle>See What CrossFit Krypton Is All About</SectionTitle>
-      <SectionSubTitle>
-        Check out this video to learn more about CrossFit Krypton
-      </SectionSubTitle>
-      <Embed
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/BIw2bx0n1o0"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      />
-      <SectionSpacer />
-      <SectionTitle>Meet the Coaches</SectionTitle>
-      <SectionSubTitle>
-        Including the 2015 CrossFit Games Champion Ben Smith
-      </SectionSubTitle>
-      <Coaches>
-        {data.allCoachesJson.edges.map(({ node }) => {
-          const { node: coachImage } = data.coachImages.edges.find(
-            ({ node: image }) =>
-              image.name.toLowerCase().includes(node.id.toLowerCase())
-          )
-          return (
-            <Coach key={node.name}>
-              <Link to={`/coaches/${node.id}`}>
-                <CoachImg
-                  alt={node.name}
-                  fluid={coachImage.childImageSharp.fluid}
+const StyledDialog = styled(DialogOverlay)`
+  display: none;
+  opacity: 0;
+  transition: opacity 1s ease;
+`
+
+const IndexPage = ({ data }) => {
+  const [showNewsletterDialog, setShowNewsletterDialog] = useState(false)
+
+  useEffect(() => {
+    const hasSeenDialog = localStorage.getItem(
+      'krypton_has_seen_newsletter_dialog'
+    )
+
+    if (!hasSeenDialog) {
+      const timer = setTimeout(() => {
+        setShowNewsletterDialog(true)
+      }, 3000)
+
+      localStorage.setItem('krypton_has_seen_newsletter_dialog', true)
+
+      return () => clearTimeout(timer)
+    }
+  }, [])
+
+  return (
+    <Layout>
+      <HeroContainer>
+        <HeroImageContainer>
+          <Carousel
+            withoutControls
+            autoplay
+            wrapAround
+            speed={600}
+            autoplayInterval={5000}
+            transitionMode="fade"
+          >
+            {data.sliderImages.edges.map(({ node: image }) => {
+              return (
+                <HeroImage
+                  key={image.name}
+                  fluid={image.childImageSharp.fluid}
                 />
-              </Link>
-              <CoachTitle>{node.name}</CoachTitle>
-              <CoachDescription>{node.short}</CoachDescription>
-            </Coach>
-          )
-        })}
-      </Coaches>
+              )
+            })}
+          </Carousel>
+        </HeroImageContainer>
+        <HeroImageOverlay />
+      </HeroContainer>
       <SectionSpacer />
-      <SectionTitle>Beyond the Whiteboard</SectionTitle>
-      <SectionSubTitle>
-        All Krypton members are given access to Beyond the White Board (BTWB)
-      </SectionSubTitle>
-      <SectionContent>
-        With BTWB members can see the next day's WOD and they can manage
-        everything online (BTW provides an excellent selection of workouts, as
-        well as all Krypton workouts). As well as manage your lifestyle and diet
-        goals along with CrossFit and easily track your progress over time.
-      </SectionContent>
-      <BtwbImageContainer>
-        <a
-          href="https://beyondthewhiteboard.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Img
-            alt={'Beyond the Whiteboard screenshot.'}
-            fluid={data.btwb.childImageSharp.fluid}
-          />
-        </a>
-      </BtwbImageContainer>
-      <SectionSpacer />
-      <SectionTitle>New to CrossFit?</SectionTitle>
-      <SectionSubTitle>See what it's all about.</SectionSubTitle>
-      <Embed
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/mlVrkiCoKkg"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      />
-    </ContentContainer>
-  </Layout>
-)
+      <ContentContainer>
+        <SectionTitle>Welcome to CrossFit Krypton</SectionTitle>
+        <SectionSubTitle>
+          We're different from your ordinary gym and gym membership
+        </SectionSubTitle>
+        <SectionContent>
+          When you come to our gym, the only thing we expect from you is a
+          willingness to learn and challenge yourself, as well as a motivation
+          and desire to better yourself each day. During each group class, we
+          will provide personal instruction, coaching, programming (exercises,
+          reps, time domains, etc.) and nutritional advice (if requested). We
+          are committed to helping every member of our gym to reach their goals,
+          and if you commit to bringing your best effort and attitude each day,
+          I can guarantee that you will exceed any realistic fitness
+          expectations you have.
+          <GetStartedButton to="/contact-us">Get Started</GetStartedButton>
+        </SectionContent>
+        <SectionSpacer />
+        <SectionTitle>See What CrossFit Krypton Is All About</SectionTitle>
+        <SectionSubTitle>
+          Check out this video to learn more about CrossFit Krypton
+        </SectionSubTitle>
+        <Embed
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/BIw2bx0n1o0"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+        <SectionSpacer />
+        <SectionTitle>Beyond the Whiteboard</SectionTitle>
+        <SectionSubTitle>
+          All Krypton members are given access to Beyond the White Board (BTWB)
+        </SectionSubTitle>
+        <SectionContent>
+          With BTWB members can see the next day's WOD and they can manage
+          everything online (BTW provides an excellent selection of workouts, as
+          well as all Krypton workouts). As well as manage your lifestyle and
+          diet goals along with CrossFit and easily track your progress over
+          time.
+        </SectionContent>
+        <BtwbImageContainer>
+          <a
+            href="https://beyondthewhiteboard.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Img
+              alt={'Beyond the Whiteboard screenshot.'}
+              fluid={data.btwb.childImageSharp.fluid}
+            />
+          </a>
+        </BtwbImageContainer>
+        <SectionSpacer />
+        <SectionTitle>New to CrossFit?</SectionTitle>
+        <SectionSubTitle>See what it's all about.</SectionSubTitle>
+        <Embed
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/mlVrkiCoKkg"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </ContentContainer>
+      <StyledDialog
+        css={
+          showNewsletterDialog
+            ? css`
+                display: block;
+                z-index: 999;
+                opacity: 1;
+              `
+            : {}
+        }
+      >
+        <DialogContent style={{ position: 'relative', borderRadius: '4px' }}>
+          <CloseDialogButton onClick={() => setShowNewsletterDialog(false)}>
+            X
+          </CloseDialogButton>
+          <SectionTitle>Don't Miss Out!</SectionTitle>
+          <SectionSubTitle>
+            Sign up for the Krypton newsletter to stay in the know about
+            everything from training camps to class times!
+          </SectionSubTitle>
+          <NewsLetterButton href="http://eepurl.com/gw9TcP">
+            Sign Up Now
+          </NewsLetterButton>
+        </DialogContent>
+      </StyledDialog>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
